cfac1a46878ccadb47afddf0d184a24273cf7fad
Jan Weidner <jw3126@gmail.com>
2024-12-05 16:02:22 +0100

    init

--- src/EnumSets.jl
+++ src/EnumSets.jl
@@ -1,5 +1,170 @@
 module EnumSets
+export @enumset
+export push
 
-# Write your package code here.
+function nbits(::Type{T}) where {T}
+    8 * sizeof(T)
+end
+
+function getbit(xT::Integer, i)::Bool
+    T = typeof(xT)
+    iT = convert(T, i)
+    (xT >> iT) & one(T)
+end
+
+function setbit(xT::Integer, i, val)
+    T = typeof(xT)
+    iT = convert(T, i)
+    valT = convert(T, val)
+    xT | (valT << iT)
+end
+
+function to_bit_index(::Type{E}, e::E) where {E}
+    for (i, v) in enumerate(instances(E))
+        if v === e
+            return i
+        end
+    end
+    error("Unreachable")
+end
+
+function from_bit_index(::Type{E}, i) where {E}
+    instances(E)[i]
+end
+
+function suggest_carriertype(E)
+    n = length(instances(E))
+    if n <= 8
+        UInt8
+    elseif n <= 16
+        UInt16
+    elseif n <= 32
+        UInt32
+    elseif n <= 64
+        UInt64
+    elseif n <= 128
+        UInt128
+    else
+        error("Enum $E does not fit into carrier type UInt128.")
+    end
+end
+
+abstract type EnumSet{E, Carrier} <: AbstractSet{E} end
+
+function _get_data(s)
+    s._data
+end
+
+function setbit(s::EnumSet, i::Int, val::Bool)::typeof(s)
+    typeof(s)(setbit(_get_data(s), i, val))
+end
+function getbit(s::EnumSet, i::Int)::Bool
+    getbit(_get_data(s), i)
+end
+
+function Base.in(e::E, s::EnumSet{E})::Bool where {E}
+    i = to_bit_index(E, e)
+    getbit(s, i)
+end
+function push(s::EnumSet{E}, e::E)::typeof(s) where {E}
+    i = to_bit_index(E, e)
+    setbit(s, i, true)
+end
+function pop(s::EnumSet{E}, e::E)::typeof(s) where {E}
+    i = to_bit_index(E, e)
+    @boundscheck if !getbit(s, i)
+        throw(KeyError(e))
+    end
+    setbit(s, i, false)
+end
+function capacity(s::EnumSet)::Int
+    8*sizeof(s)
+end
+function Base.length(s::EnumSet)::Int
+    count_ones(s._data)
+end
+function Base.iterate(s::EnumSet{E}, i::Int=1)::Union{Nothing, Tuple{E, Int}} where {E}
+    c = capacity(s)
+    while i <= c
+        if getbit(s, i)
+            return from_bit_index(E, i), i + 1
+        else
+            i += 1
+        end
+    end
+    return nothing
+end
+
+function Base.union(s1::S, ss::S...)::S where {S <: EnumSet}
+    S((|)(_get_data(s1), map(_get_data, ss)...))
+end
+
+function Base.intersect(s1::S, ss::S...)::S where {S <: EnumSet}
+    S((&)(_get_data(s1), map(_get_data, ss)...))
+end
+
+function make_error_msg(ex)
+    """
+    $ex is not an expression of the form
+    MySet <: EnumSet{MyEnum}
+    MySet <: EnumSet{MyEnum, UInt64}
+    """
+end
+
+macro enumset(ex)
+    if !Meta.isexpr(ex, :<:)
+        error(make_error_msg(ex))
+    end
+    ESet, ex_EnumSet = ex.args
+    if !Meta.isexpr(ex_EnumSet, :curly)
+        error(make_error_msg(ex_EnumSet))
+    end
+    if length(ex_EnumSet.args) == 2
+        symbol_EnumSet, E = ex_EnumSet.args
+        Carrier = nothing
+    elseif length(ex_EnumSet.args) == 3
+        symbol_EnumSet, E, Carrier = ex_EnumSet.args
+    else
+        error(make_error_msg(ex_EnumSet))
+    end
+    E = __module__.eval(E)
+    Carrier = __module__.eval(Carrier)
+    if isnothing(Carrier)
+        Carrier = suggest_carriertype(E)
+    end
+    esc(enumsetmacro(E, ESet, Carrier))
+end
+
+function enumsetmacro(E::Type, ESet::Symbol, Carrier::Type)
+
+    if length(instances(E)) > nbits(Carrier)
+        msg = """
+        Enum $E does not fit into carrier type $Carrier.
+        length(instances($E)) = $(length(instances(E)))
+        nbits($Carrier)       = $(nbits(Carrier))
+        """
+    end
+    quote
+        struct $ESet <: $EnumSet{$E, $Carrier}
+            _data::$Carrier
+            function $ESet(carrier::$Carrier)
+                new(carrier)
+            end
+        end
+        function $ESet()::$ESet
+            $ESet(zero($Carrier))
+        end
+        function $ESet(e::$ESet)::$ESet
+            e
+        end
+        function $ESet(itr)::$ESet
+            ret = $ESet()
+            for e in itr
+                ret = $push(ret, convert($E, e))
+            end
+            ret
+        end
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,132 @@
 using EnumSets
 using Test
 
-@testset "EnumSets.jl" begin
-    # Write your tests here.
+@enum Alphabet begin
+    A=1 
+    B=2 
+    C=3
+end
+
+@testset "simple enum" begin
+    @enumset ASet <: EnumSet{Alphabet}
+
+    s = ASet()
+    @test isbitstype(ASet)
+    @test isempty(s)
+    @test length(s) == 0
+    sA = push(s, A)
+    @test length(sA) == 1
+    @test A in sA
+    @test !(B in sA)
+    @test !(C in sA)
+    @test sA === push(sA, A)
+
+    sAC = push(sA, C)
+    @test A in sAC
+    @test !(B in sAC)
+    @test (C in sAC)
+    @test collect(sAC) == [A, C]
+
+    @test ASet([A,B]) != ASet([B])
+    @test ASet([A,B]) === ASet([B, A])
+    @test union(ASet([A]), ASet([B])) == ASet([A, B])
+    @test union(ASet([A]), ASet([B]), ASet([A,B])) == ASet([A, B])
+
+    @show intersect(ASet([A,B]), ASet([C,B]))
+    @test intersect(ASet([A,B]), ASet([C,B])) === ASet([B])
+end
+
+@enum Negative::Int128 begin
+    a=0
+    b=-1
+    c=-3
+    d=typemin(Int128)
+    t=typemax(Int128)
+end
+
+@enumset NegativeSet <: EnumSet{Negative}
+
+@enum ProgrammerExcuse begin
+    WorksOnMyMachine
+    MercurialRetrograde
+    CosmicRayBitFlip
+    CoffeeNotFound
+    KeyboardNotErgonomic
+    WrongMoonPhase
+    CatOnKeyboard
+    StandupMeetingTooLong
+    BadrouterIonization
+    CompilerFeelingMoody
+    CacheTooCachey
+    NotEnoughEmojis
+    TooManyEmojis
+    QuotaExceededForSighs
+    WifiPasswordTooStrong
+    DnsLookingOtherWay
+    FirewallTooFirey
+    CloudsTooFluffy
+    GitBranchTooBranchy
+    StackOverflowDown
+    RedditWasDistracting
+    SlackNotSlacking
+    ZoomBackgroundTooVirtual
+    MousepadTooSlippery
+    ChairNotGaming
+    DeskTooClean
+    DeskTooMessy
+    MonitorNotVertical
+    SecondMonitorJealous
+    ThirdMonitorMissing
+    RubberDuckOnVacation
+    TerminalTooColourful
+    FontNotCoding
+    TabsVsSpacesDispute
+    GitConflictTooConflicting
+    LinuxKernelTooKernelly
+    WindowsUpdateConspiracy
+    MacBookTooShiny
+    DockerWhaleNotSwimming
+    KubernetesPodsHomesick
+    JiraTicketEscaped
+    BacklogTooLogged
+    SprintTooMarathon
+    ScrumTooChaotic
+    WaterfallTooWet
+    AgileNotNimble
+    TechnicalDebtCollector
+    LegacyCodeHaunting
+    UnitTestsTooUnity
+    IntegrationTestsDancing
+    E2ETestsDaydreaming
+    CICDPipelineClogged
+    DevOpsSleeping
+    ProdEnvAngry
+    StagingStaged
+    TestEnvTesting
+    LocalhostLost
+    PortsAllBusy
+    MemoryLeakingSlowly
+    CPUOnStrike
+    GPUMining
+    RAMDownloading
+    SSLCertificateShy
+    APITooRESTful
+    GraphQLTooGraphy
+    WebsocketDisconnected
+end
+
+@testset "big enum" begin
+    @test length(instances(ProgrammerExcuse)) > 64
+    @enumset ProgrammerExcuseSet <: EnumSet{ProgrammerExcuse}
+    @test 8*sizeof(ProgrammerExcuseSet) >= length(instances(ProgrammerExcuse))
+    s = ProgrammerExcuseSet()
+    @test isempty(s)
+    @test length(s) == 0
+    for (i,e) in enumerate(instances(ProgrammerExcuse))
+        @test !(e in s)
+        s = push(s, e)
+        @test e in s
+        @test length(s) == i
+        @test collect(s) == collect(instances(ProgrammerExcuse))[1:i]
+    end
 end
